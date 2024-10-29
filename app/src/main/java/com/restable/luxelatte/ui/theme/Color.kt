3130ff9833aa6cce45e7f7a9641a481eb03bd1df
Hiran Lakshika <hiranlakshika@gmail.com>
2024-10-29 11:28:32 +0100

    Fonts and colors

--- app/src/main/java/com/restable/luxelatte/ui/theme/Color.kt
+++ app/src/main/java/com/restable/luxelatte/ui/theme/Color.kt
@@ -2,9 +2,15 @@ package com.restable.luxelatte.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Light Theme Colors
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
+val primaryBrown = Color(0xFF4E2525)
+val primaryCard = Color(0xFFF7F1F1)
+val primaryButton = Color(0xFFD9D9D9)
+val secondaryButton = Color(0xFFC67C4E)
+val ratingStar = Color(0xFFFFC01E)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
--- app/src/main/java/com/restable/luxelatte/ui/theme/Type.kt
+++ app/src/main/java/com/restable/luxelatte/ui/theme/Type.kt
@@ -1,34 +1,42 @@
 package com.restable.luxelatte.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.sp
+import com.restable.luxelatte.R
 
-// Set of Material typography styles to start with
+val kaushanScriptFont = FontFamily(Font(R.font.kaushan_script_regular, FontWeight.Bold))
+val interFont = FontFamily(Font(R.font.inter_regular, FontWeight.Bold))
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontFamily = kaushanScriptFont,
+        fontWeight = FontWeight.W400,
+        fontSize = 48.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.5.sp,
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Proportional,
+            trim = LineHeightStyle.Trim.Both
+        ),
+        platformStyle = PlatformTextStyle(includeFontPadding = true)
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+    bodyLarge = TextStyle(
+        fontFamily = interFont, fontWeight = FontWeight.W400, fontSize = 20.sp,
+        lineHeight = 22.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = interFont, fontWeight = FontWeight.W400, fontSize = 15.sp,
+        lineHeight = 22.sp,
+    ),
+    bodySmall = TextStyle(
+        fontFamily = interFont, fontWeight = FontWeight.W400, fontSize = 12.sp,
+        lineHeight = 22.sp,
+    ),
 )
